cf0f1409b3434b83e4d24cc1ead911a37630a7fc
emiko <31835314+siroemk@users.noreply.github.com>
2022-03-03 12:56:30 +0900

    メモ一覧を作り、メモを追加できるようにした

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <PageTitle msg="Memo App"/>
+  <div id="app">
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">{{ memo.text }}</li>
+      <button type="button" @click="displayForm">+</button>
+    </ul>
+  </div>
+  <div v-if="edit">
+    <input class="form" v-model="text">
+    <button type="button" @click="addMemo">編集</button>
+    <button type="button">削除</button>
+  </div>
 </template>
 
 <script>
@@ -9,6 +20,27 @@ export default {
   name: 'App',
   components: {
     PageTitle
+  },
+  data() {
+    return{
+      memos: JSON.parse(localStorage.getItem('memos')),
+      text: '',
+      edit: false
+    }
+  },
+  methods: {
+    addMemo: function() {
+      this.memos.push({
+        id: new Date().getTime().toString(),
+        text: this.text,
+      })
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+      this.edit = false
+    },
+    displayForm: function() {
+      this.edit = true
+      this.text = '新規メモ'
+    }
   }
 }
 </script>
