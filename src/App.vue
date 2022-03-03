0f1ee4f6c1fe0b0fd8f85156b45d5e089655db2c
emiko <31835314+siroemk@users.noreply.github.com>
2022-03-03 14:24:43 +0900

    メモの編集をできるようにした

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <PageTitle msg="Memo App"/>
   <div id="app">
     <ul>
-      <li v-for="memo in memos" :key="memo.id">{{ memo.text }}</li>
-      <button type="button" @click="displayForm">+</button>
+      <li v-for="memo in memos" :key="memo.id" @click="displayEditForm(memo)">{{ memo.text }}</li>
+      <button type="button" @click="addMemo">+</button>
     </ul>
   </div>
   <div v-if="edit">
     <input class="form" v-model="text">
-    <button type="button" @click="addMemo">編集</button>
+    <button type="button" @click="editMemo">編集</button>
     <button type="button">削除</button>
   </div>
 </template>
@@ -25,21 +25,30 @@ export default {
     return{
       memos: JSON.parse(localStorage.getItem('memos')),
       text: '',
-      edit: false
+      edit: false,
+      targetMemo: ''
     }
   },
   methods: {
     addMemo: function() {
       this.memos.push({
         id: new Date().getTime().toString(),
-        text: this.text,
+        text: '新規メモ',
       })
       localStorage.setItem('memos', JSON.stringify(this.memos))
-      this.edit = false
+      const memo = this.memos.slice(-1)[0]
+      this.displayEditForm(memo)
     },
-    displayForm: function() {
+    editMemo: function() {
+      const index = this.memos.indexOf(this.targetMemo)
+      const memoToEdit = this.memos[index]
+      memoToEdit['text'] = this.text
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+    },
+    displayEditForm: function(memo) {
       this.edit = true
-      this.text = '新規メモ'
+      this.text = memo.text
+      this.targetMemo = memo
     }
   }
 }
